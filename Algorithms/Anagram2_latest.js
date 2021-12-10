5b158a3da2042733cc278abf32a4092b55f3bce3
Vardan GHULYAN <vardanghulyan@macbook-pro.home>
2021-12-10 14:05:52 +0000

    Added the latest anagram

---
+++ Algorithms/Anagram2_latest.js
@@ -0,0 +1,42 @@
+
+// characters based
+detectAnagram(one: any, two:any) {
+  const sortedWord = one
+    .replace(/ /g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+  const sortedWordTwo = two
+    .replace(/ /g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+
+    return sortedWord === sortedWordTwo
+ }
+
+ detectAnagrams('giraffe', '   G   ira  f fe')
+
+ // word base
+ var input = "monk, konm, bbc, cbb, dell, ledl";
+var words = input.split(", ");
+
+for (var i = 0; i < words.length; i++) {
+
+  var word = words[i];
+  var alphabetical = word.split("").sort().join("");
+
+  for (var j = 0; j < words.length; j++) {
+
+    if (i === j) {
+      continue;
+    }
+
+    var other = words[j];
+    if (alphabetical === other.split("").sort().join("")) {
+      console.log(word + " - " + other + " (" + i + ", " + j + ")");
+    }
+  }
+}
